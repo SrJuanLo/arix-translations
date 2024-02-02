c4cdf4292791f0db345097764562816b1f0db85c
SrJuanLo <158538150+SrJuanLo@users.noreply.github.com>
2024-02-02 01:03:04 -0300

    Spanish/Español
    Arix Theme traducido al Español por Sr. JuanLo

--- server/network.php
+++ server/network.php
@@ -1,18 +1,18 @@
 <?php
 
-return[
-    'network' => 'Network',
-    'manage-allocation' => 'Manage allocation',
-    'create-allocation' => 'Create Allocation',
-    'currently-using' => 'You are currently using {{current}} of {{max}} allowed allocations for this server.',
+return [
+    'network' => 'Red',
+    'manage-allocation' => 'Gestionar asignación',
+    'create-allocation' => 'Crear asignación',
+    'currently-using' => 'Actualmente estás utilizando {{current}} de {{max}} asignaciones permitidas para este servidor.',
 
     'IP' => 'IP',
-    'port' => 'Port',
-    'notes' => 'Notes',
-    'primary' => 'Primary',
-    'make-primary' => 'Make Primary',
+    'port' => 'Puerto',
+    'notes' => 'Notas',
+    'primary' => 'Primario',
+    'make-primary' => 'Hacer primario',
 
-    'remove-allocation' => 'Remove Allocation',
-    'remove-allocation-description' => 'This allocation will be immediately removed from your server.',
-    'delete' => 'Delete'
+    'remove-allocation' => 'Eliminar asignación',
+    'remove-allocation-description' => 'Esta asignación se eliminará inmediatamente de tu servidor.',
+    'delete' => 'Eliminar'
 ];
