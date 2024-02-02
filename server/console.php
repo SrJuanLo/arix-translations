593685cc00c2da94a6cd5d04314d702a8b418438
SrJuanLo <158538150+SrJuanLo@users.noreply.github.com>
2024-02-02 00:57:25 -0300

    Spanish/Español
    Arix Theme traducido al Español por Sr. JuanLo

--- server/console.php
+++ server/console.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'console' => 'Console',
-    'node-under-maintenance' => 'The node of this server is currently under maintenance and all actions are unavailable.',
-    'running-installation-process' => 'This server is currently running its installation process and most actions are unavailable.',
-    'being-transferred' => 'This server is currently being transferred to another node and all actions are unavailable.',
-    'type-a-command' => 'Type a command...',
+    'console' => 'Consola',
+    'node-under-maintenance' => 'El nodo de este servidor está actualmente en mantenimiento y todas las acciones no están disponibles.',
+    'running-installation-process' => 'Este servidor está actualmente ejecutando su proceso de instalación y la mayoría de las acciones no están disponibles.',
+    'being-transferred' => 'Este servidor está siendo transferido a otro nodo y todas las acciones no están disponibles.',
+    'type-a-command' => 'Escribe un comando...',
 ];
